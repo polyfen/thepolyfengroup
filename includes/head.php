0c17d02e935629a7ea4ee23b4ee18d489a1f2ffb
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-08-22 22:25:10 +0200

    Initial commit

---
+++ includes/head.php
@@ -0,0 +1,30 @@
+<!-- META General -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta property="og:type" content="website"/>
+<meta name="author"  content="The Polyfen Group"/>
+
+<!-- CSS General -->
+<link rel="stylesheet" href="https://polynucleus.com/polynucleus.css" type="text/css">
+<link rel="stylesheet" href="app/styles.css" type="text/css">
+
+<!-- FONTS -->
+<!--
+/**
+* @license
+* MyFonts Webfont Build ID 568563
+*
+* The fonts listed in this notice are subject to the End User License
+* Agreement(s) entered into by the website owner. All other parties are
+* explicitly restricted from using the Licensed Webfonts(s).
+*
+* You may obtain a valid license from one of MyFonts official sites.
+* http://www.fonts.com
+* http://www.myfonts.com
+* http://www.linotype.com
+*
+*/
+-->
+<link rel="stylesheet" type="text/css" href="app/fonts/Biotif.css">
+<link rel="stylesheet" href="https://use.typekit.net/aba8ecz.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>The Polyfen Group</title>
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+  </head>
+
+  <body>
+    <header>
+        <img src="imgs/the-polyfen-group-logo.svg">
+    </header>
+    <main>
+        <nav>
+            <a href="">Business Plan</a>
+            <a href="">Brand Guidelines</a>
+        </nav>
+        <section id="cover">
+            <h2 class="title-3">We explore new ways of using tech in creative projects.</h2>
+        </section>
+        <section id="sub-brands-container">
+            <h2 class="heading-1">Our Brands</h2>
+            <section class="sub-brand">
+                <img src="imgs/sub-brands/polyfen-logo.svg" alt="Polyfen">
+                <p>Creative agency offering Brand Identity Design services for new and emerging businesses.</p>
+                <a href="https://polyfen.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyfen.com</a>
+            </section>
+            <hr>
+            <section class="sub-brand">
+                <img src="imgs/sub-brands/poly-atlas-logo.svg" alt="Poly Atlas">
+                <p>Open-source step-by-step guide to the Branding process, including practical tips, case studies, template files, and many other useful resources.</p>
+                <a href="https://polyatlas.wiki" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyatlas.wiki</a>
+            </section>
+            <hr>
+            <section class="sub-brand">
+                <img src="imgs/sub-brands/poly-forum-logo.svg" alt="Poly Forum">
+                <p>Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry.</p>
+                <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyforum.org</a>
+            </section>
+            <hr>
+            <section class="sub-brand">
+                <img src="imgs/sub-brands/poly-cookies-logo.svg" alt="Poly Cookies">
+                <p>Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager.</p>
+                <a href="https://polycookies.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polycookies.com</a>
+            </section>
+            <hr>
+            <section class="sub-brand" style="display:none;">
+                <img src="imgs/sub-brands/poly-qwerty-logo.svg" alt="Poly Qwerty">
+            </section>
+        </section>
+    </main>
+    <footer></footer>
+  </body>
+</html>
