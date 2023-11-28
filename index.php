5cdcdb2abf901f8f9c661f4394c99efe1d323e95
xxiicode <leandrobilokapic@gmail.com>
2023-11-28 12:23:13 +0100

    alt=""

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
                     <a href="https://polyfen.com" target="_blank" class="web-url">Polyfen<span></span></a>
                 </h3>
                 <p>Creative agency offering design, marketing, and software development services for new and emerging businesses.</p>
-                <a href="https://polyfen.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon"  alt="">polyfen.com</a>
+                <a href="https://polyfen.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon" alt="">polyfen.com</a>
             </section>
             <hr>
             <section class="sub-brand">
@@ -60,7 +60,7 @@
                     <a href="https://polyatlas.wiki" target="_blank" class="web-url">Polyatlas<span></span></a>
                 </h3>
                 <p>Open-source step-by-step guide to the Branding process, including practical tips, case studies, template files, and many other useful resources.</p>
-                <a href="https://polyatlas.wiki" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon"  alt="">polyatlas.wiki</a>
+                <a href="https://polyatlas.wiki" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon" alt="">polyatlas.wiki</a>
             </section>
             <hr>
             <section class="sub-brand">
@@ -68,7 +68,7 @@
                     <a href="https://polycookies.com" target="_blank" class="web-url">Polycookies<span></span></a>
                 </h3>
                 <p>Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager.</p>
-                <a href="https://polycookies.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polycookies.com</a>
+                <a href="https://polycookies.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon" alt="">polycookies.com</a>
             </section>
             <hr>
             <section class="sub-brand">
@@ -76,7 +76,7 @@
                     <a href="https://polyforum.org" target="_blank" class="web-url">Polyforum<span></span></a>
                 </h3>
                 <p>Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry.</p>
-                <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyforum.org</a>
+                <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon" alt="">polyforum.org</a>
             </section>
             <hr>
         </section>
@@ -88,7 +88,7 @@
                     <a href="https://polyqwerty.com" target="_blank" class="web-url">polyqwerty<span></span></a>
                 </h3>
                 <p>Your keyboard's assistant. Click on a character to copy it.</p>
-                <a href="https://polyqwerty.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyqwerty.com</a>
+                <a href="https://polyqwerty.com" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon" alt="">polyqwerty.com</a>
             </section>
             <hr>
             <section class="sub-brand">
