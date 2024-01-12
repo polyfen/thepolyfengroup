1a86ed82d3bc5af03157e70303723d8d390bcb9c
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-12 21:11:12 +0100

    Update team-messaging.php

--- wiki/content/team-messaging.php
+++ wiki/content/team-messaging.php
@@ -4,41 +4,60 @@
 <ol class="hanging-list">
     <li>Be respectful and considerate.</li>
     <li>Use appropriate channels. If you don't know where to post a message, then you can ask anyone for clarification via DM (direct message) or you can post your question in our #help channel.</li>
-    <li>When discussing times and dates be sure to clarify which timezone you're talking about.</li>
-    <li>If you have any suggestions for our rules we encourage you to participate and let us know your thoughts!</li>
+    <li>When discussing times and dates be sure to clarify which timezone you're referring to.</li>
+    <li>If you have any suggestions for our rules we encourage you to participate and let us know how we can make this better for everyone.</li>
 </ol>
 <h3 class="heading-2">3.2. Channels</h3>
 <p style="margin-bottom: 0;"><strong>General:</strong></p>
 <ul class="hanging-list arrow-bullet">
-    <li>#announcements</li>
-    <li>#help</li>
-    <li>#offtopic-chatter</li>
-    <li>#team-wiki</li>
+    <li>#announcements<br>
+    000</li>
+    <li>#help<br>
+    000</li>
+    <li>#offtopic-chatter<br>
+    000</li>
+    <li>#team-wiki<br>
+    000</li>
+    <li>#coworking<br>
+    000</li>
 </ul>
 
 <p style="margin-bottom: 0;"><strong>Education:</strong></p>
 <ul class="hanging-list arrow-bullet">
-    <li>#design-education</li>
-    <li>#frontend-education</li>
+    <li>#design-education<br>
+    000</li>
+    <li>#frontend-education<br>
+    000</li>
 </ul>
 
 <p style="margin-bottom: 0;"><strong>Projects:</strong></p>
 <ul class="hanging-list arrow-bullet">
-    <li>#polyfen</li>
-    <li>#poly-atlas</li>
-    <li>#poly-cookies</li>
-    <li>#poly-qwerty</li>
-    <li>#poly-kit</li>
+    <li>#polyfen<br>
+    000</li>
+    <li>#poly-atlas<br>
+    000</li>
+    <li>#poly-cookies<br>
+    000</li>
+    <li>#poly-qwerty<br>
+    000</li>
+    <li>#polykit<br>
+    000</li>
 </ul>
 
 <p style="margin-bottom: 0;"><strong>Divisions:</strong></p>
 <ul class="hanging-list arrow-bullet">
-    <li>#administrative</li>
-    <li>#business</li>
-    <li>#design</li>
-    <li>#marketing</li>
-    <li>#engineering</li>
-    <li>#board</li>
+    <li>#administrative<br>
+    000</li>
+    <li>#business<br>
+    000</li>
+    <li>#design<<br>
+    000/li>
+    <li>#marketing<br>
+    000</li>
+    <li>#engineering<br>
+    000</li>
+    <li>#board<br>
+    000</li>
 </ul>
 
 </section>
