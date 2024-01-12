0c08e2518d171b0c7d7d9ef515f2b6e61657f2c4
xxiicode <leandrobilokapic@gmail.com>
2024-01-12 19:06:50 +0100

    privacy

---
+++ privacy/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Privacy Policy | Polyfen</title>
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+  </head>
+
+  <body>
+    <nav id="navbar">
+      <h2 class="caption">Table of Contents</h2>
+      <ul>
+        <li><a href="#" class="caption">Privacy Policy</a></li>
+        <li><a href="#definitions-and-key-terms" class="caption">1. Definitions &amp; Key Terms</a></li>
+        <li><a href="#information-we-collect" class="caption">2. Information We Collect</a></li>
+        <li><a href="#how-we-use-your-information" class="caption">3. How We Use Your Information</a></li>
+        <li><a href="#when-we-use-your-information" class="caption">4. When We Use Your Information</a></li>
+        <li><a href="#when-we-use-third-parties" class="caption">5. When We Use Third Parties</a></li>
+        <li><a href="#how-your-information-may-be-shared" class="caption">6. How Your Information May Be Shared</a></li>
+        <li><a href="#where-we-collect-information" class="caption">7. When & Where We Collect Information</a></li>
+        <li><a href="#how-we-user-your-email-address" class="caption">8. How We User Your Email Address</a></li>
+        <li><a href="#how-long-do-we-keep-your-information" class="caption">9. How Long Do We Keep Your Information</a></li>
+        <li><a href="#how-we-protect-your-information" class="caption">10. How We Protect Your Information</a></li>
+        <li><a href="#my-information-in-other-countries" class="caption">11. My Information in Other Countries</a></li>
+        <li><a href="#is-the-information-secure" class="caption">12. Is The Information Secure?</a></li>
+        <li><a href="#editing-my-information" class="caption">13. Editing My Information</a></li>
+        <li><a href="#personnel" class="caption">14. Personnel</a></li>
+        <li><a href="#sale-of-business" class="caption">15. Sale of Business</a></li>
+        <li><a href="#affiliates" class="caption">16. Affiliates</a></li>
+        <li><a href="#governing-law" class="caption">17. Governing Law</a></li>
+        <li><a href="#your-consent" class="caption">18. Your Consent</a></li>
+        <li><a href="#links-to-other-websites" class="caption">19. Links to Other Websites</a></li>
+        <li><a href="#cookies" class="caption">20. Cookies</a></li>
+        <li><a href="#blocking-and-disabling-tracking" class="caption">21. Blocking and Disabling Tracking</a></li>
+        <li><a href="#kids-privacy" class="caption">22. Kid's Privacy</a></li>
+        <li><a href="#changes-to-our-privacy-policy" class="caption">23. Changes To Our Privacy Policy</a></li>
+        <li><a href="#third-party-services" class="caption">24. Third-Party Services</a></li>
+        <li><a href="#facebook-pixel" class="caption">25. Facebook Pixel</a></li>
+        <li><a href="#tracking-technologies" class="caption">26. Tracking Technologies</a></li>
+        <li><a href="#what-is-gdpr" class="caption">27. What is GDPR?</a></li>
+        <li><a href="#what-is-personal-data" class="caption">28. What is personal data?</a></li>
+        <li><a href="#why-is-gdpr-important" class="caption">29. Why is GDPR Important?</a></li>
+        <li><a href="#individual-data-subjects-rights" class="caption">30. Individual Data Subject's Rights</a></li>
+        <li><a href="#california-residents" class="caption">31. California Residents</a></li>
+        <li><a href="#california-online-privacy-protection-act" class="caption">32. California Online Privacy</a></li>
+        <li><a href="#contact-us" class="caption">33. Contact Us</a></li>
+        <br><br>
+      </ul>
+    </nav>
+    <button aria-label="Open/Close navbar" class="hamburger">
+        <img src="imgs/icons/hamburger_icon.svg" alt="" height="30px" width="auto">
+    </button>
+    <button class="close">
+        <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24">
+          <path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16 16l-4-4m0 0L8 8m4 4l4-4m-4 4l-4 4"/>
+        </svg>
+    </button>
+    <main>
+      <section class="cover">
+        <img class="plfn-logo" src="imgs/polyfen-logo.svg" alt="Polyfen" height="80px" width="auto">
+        <img src="imgs/padlock-illustration.svg" alt="Padlock Illustration" height="300px" width="auto">
+        <h1 class="title-2">Privacy Policy</h1>
+        <h2 class="caption">The Polyfen Group LLC<br>Est. 2018</h2>
+      </section>
+        <hr>
+        <!-- INCLUDE CONTENT.PHP start  -->
+        <?php include 'content/content.php';?>
+        <!-- INCLUDE CONTENT.PHP end -->
+        <hr>
+    </main>
+  </body>
+</html>
