eac26e965deaab03ac7fe6076519405a7197b933
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-13 18:52:58 +0100

    Content update

--- brand/content/brand-strategy.php
+++ brand/content/brand-strategy.php
@@ -1,9 +1,6 @@
 <section id="brand-strategy">
 <h2 class="heading-1">3. Brand Strategy</h2>
-<h3 class="heading-2">3.1. Voice</h3>
-<p>The content expressed by our brand must be informative and engaging, proactive in the design community.</p>
-<h3 class="heading-2">3.2. Tone</h3>
-<h3 class="heading-2">3.3. Keywords</h3>
+<h3 class="heading-2">3.1. Keywords</h3>
 <ul class="hanging-list arrow-bullet">
     <li>Radical</li>
     <li>Creative</li>
@@ -14,8 +11,10 @@
     <li>Distinctive</li>
     <li>Business-oriented</li>
 </ul>
-<h3 class="heading-2">3.4. Creative Concept</h3>
-<h4 class="heading-3">Where business & creativity come together</h4>
+<h3 class="heading-2">3.2. Creative Concept</h3>
+<div class="preview">
+    <h4 class="heading-3 align-center" style="max-width:500px;margin:0 auto;padding-top:0;">Where business & creativity come together</h4>
+</div>
 <p>Similar to the Keywords, but more expressive and distinctive, the Creative Concept is a word or phrase that represents the desired emotional response from the target public when interacting with the Brand.</p>
 <p>Unlike a Slogan, a Creative Concept is not necessarily developed for public use, although it can be used publicly. Its primary purpose is to be used internally as a mantra to guide the creative process when sketching a Logo, copywriting a slogan, designing an ad, or when developing any other type of Brand Communication. This ensures that all elements of the Brand will convey a consistent message.</p>
 <h3 class="heading-2">3.5. Moodboard</h3>
@@ -38,4 +37,6 @@ Preferred name for use on all official business documentation.</p>
 TPG / Poly<br/>
 Casual diminutive alternative for internal reference within the team.</p>
 <h3 class="heading-2">3.7. Brand Architecture</h3>
+<h3 class="heading-2">3.1. Voice</h3>
+<p>The content expressed by our brand must be informative and engaging, proactive in the design and development community.</p>
 </section>
