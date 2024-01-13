17b832af0b56ad5936b1762cf30285814c9f0da2
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-13 18:00:10 +0100

    Update market-research.php

--- brand/content/market-research.php
+++ brand/content/market-research.php
@@ -8,4 +8,135 @@
 <img src="imgs/polyfen-product-canvas.png" alt="Polyfen Product Canvas">
 <h3 class="heading-2">2.2. Industry Analysis</h3>
 <h3 class="heading-2">2.3. Target Analysis</h3>
+<table>
+    <th>Buyer Persona No.1</th>
+    <tr>
+        <th>Portrait</th>
+        <td><img src="#" alt=""></td>
+    </tr>
+    <tr>
+        <th>Pseudonym</th>
+        <td>Founder Frank</td>
+    </tr>
+    <tr>
+        <th>Occupation</th>
+        <td>Founder & CEO at Business Management Consultancy</td>
+    </tr>
+    <tr>
+        <th>Location</th>
+        <td>San Francisco, California</td>
+    </tr>
+    <tr>
+        <th>Company Size</th>
+        <td>8 employees</td>
+    </tr>
+    <tr>
+        <th>Brands they follow</th>
+        <td><p>Forbes, Bloomberg, Business Insider, Inc., Ted Talks, Ideo, Deloitte, Accenture, The Wall Street Journal, The Economist, Harvard Business Review, Entrepreneur.</p></td>
+    </tr>
+    <tr>
+        <th>Bio</th>
+        <td><p>Frank has an Ivy League education on business, and he counts with 15+ years of experience in business. He was leading a business development team at Accenture, working on projects for Nike until, during the uncertain time of the Covid pandemic, he decided to make use of his gathered professional experience to start a venture of his own. He bootstraped his initial efforts investing money from his own pocket into his business project. He came up with a name for his company himself, he registered the webdomain of the company himself, he posted a job on Upwork to get a quick logo done by a cheap designer, and he created his business' website himself on Wix. He reached out to a few close, like-minded, experienced colleagues with whom he has worked previously on other business and offered them to be a part of the team he's leading. After a couple of months of launching his business, he closed a long-term contract for continuous consulting services for a large corporation. This allowed him to expand his team hiring an executive assistant and formalize other aspects of his business like bookkeeping so he doesn't have to take care of everything completely by himself. Now that the business has some dough and a couple of regular clients he wants to scale his operation to take in new, bigger clients to really make a difference in the income the consulting firm generates. However, as a savvy business person, he realizes that the quick logo that he got done for cheap and the Wix website he created himself do not express the level of professionalism he's hoping to communicate to potential clients.</p></td>
+    </tr>
+    <tr>
+        <th>Problem</th>
+        <td><p>He is the Founder & CEO of an emerging company in the Business Consulting sector but their Brand Identity doesn’t reflect the high quality of their services.</p></td>
+    </tr>
+    <tr>
+        <th>Solution</th>
+        <td><p>(End-to-End Project contract) Finding a reliable team of experts who can translate his vision for the business into a compelling brand identity and a website with user experience that delivers a good, professional, trustworthy impression to prospects and interests them in becoming paying clients.</p></td>
+    </tr>
+    <tr>
+        <th>How will they measure success?</th>
+        <td><p>Having an increase in the number of meetings scheduled with leads every month.</p></td>
+    </tr>
+</table>
+<hr class="second-separator">
+<table>
+    <th>Buyer Persona No.2</th>
+    <tr>
+        <th>Portrait</th>
+        <td><img src="#" alt=""></td>
+    </tr>
+    <tr>
+        <th>Pseudonym</th>
+        <td>Corporate Catherine</td>
+    </tr>
+    <tr>
+        <th>Occupation</th>
+        <td>CMO at Fintech App</td>
+    </tr>
+    <tr>
+        <th>Location</th>
+        <td>Houston, Texas</td>
+    </tr>
+    <tr>
+        <th>Company Size</th>
+        <td>65 employees</td>
+    </tr>
+    <tr>
+        <th>Brands they follow</th>
+        <td><p>#</p></td>
+    </tr>
+    <tr>
+        <th>Bio</th>
+        <td><p>Catherine has a background in marketing with a strong focus on closing new clients and upselling existing ones through social media, PR, and email marketing campaigns. In previous jobs, she helped startups get out the door and make the most use of their funding to establish themselves in the market and fulfill their expected ROI. Three years ago, the head of HR of a young fintech startup reached out to her, offering the role of CMO. Since then, she succesfully put new marketing strategies into action that showed significantly positive ROI results with an exponential increase in registrations of new users. However, most of the in-house staff at the company are heavily oriented in developing new features for the product, and she realizes that the visual identity of the brand has suffered from neglect at the hand of a creative team that is strong in UX, but not in branding and visual design. The logo of the app is OK, but it's not great. The UI has inconsistencies in the use of color and typography, and the set of illustrations they use looks too generic. With the number of sign-ups and retention recently stalling, she proposed to the C-level to refresh their brand with a more eye-pleasing visual identity to make the app more attractive over its competitors and show a more promising value for its price. However, her team is not particularly strong in branding; plus, they could use a fresh set of eyes.</p></td>
+    </tr>
+    <tr>
+        <th>Problem</th>
+        <td><p>#</p></td>
+    </tr>
+    <tr>
+        <th>Solution</th>
+        <td><p>(Executive Consulting contract)</p></td>
+    </tr>
+    <tr>
+        <th>How will they measure success?</th>
+        <td><p>Increase in the number of monthly sign-ups and lower churn rate.</p></td>
+    </tr>
+</table>
+<hr class="second-separator">
+<table>
+    <th>Buyer Persona No.3</th>
+    <tr>
+        <th>Portrait</th>
+        <td><img src="#" alt=""></td>
+    </tr>
+    <tr>
+        <th>Pseudonym</th>
+        <td>Director Dave</td>
+    </tr>
+    <tr>
+        <th>Occupation</th>
+        <td>Managing Director at Marketing Agency</td>
+    </tr>
+    <tr>
+        <th>Location</th>
+        <td>Miami, Florida</td>
+    </tr>
+    <tr>
+        <th>Company Size</th>
+        <td>24 employees</td>
+    </tr>
+    <tr>
+        <th>Brands they follow</th>
+        <td><p>#</p></td>
+    </tr>
+    <tr>
+        <th>Bio</th>
+        <td><p>Dave is a self-made man. He didn't went to a top college, but he still committed himself fully to his professional career burning the midnight oil to get new clients for a marketing agency where he had a junior sales rep role. He climbed up the corporate ladder being head-hunted for better and better positions. He developed wonderful relationships with loyal clients and made himself a name in the industry. One year ago, the founder and CEO of new marketing agency he used to work with as a client offered him a steady position at the company being his right-hand man in keeping the revenue growing.</p></td>
+    </tr>
+    <tr>
+        <th>Problem</th>
+        <td><p>The inhouse team already has a heavy workload and the C-level wants to keep growing the revenue with new clients but they don't want to hire a bunch of new employees and have to deal with the management and budgeting risks that come with scaling the business.</p></td>
+    </tr>
+    <tr>
+        <th>Solution</th>
+        <td><p>(Staff Augmentation contract)</p></td>
+    </tr>
+    <tr>
+        <th>How will they measure success?</th>
+        <td><p>Customers commenting to accounts executives that they are very satisfied with their results and want to continue working with the Marketing Agency.</p></td>
+    </tr>
+</table>
 </section>
