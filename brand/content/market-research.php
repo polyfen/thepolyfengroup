1a930b808178ff0677e8922df880e504bb2f94d7
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-13 17:42:03 +0100

    Market research update

--- brand/content/market-research.php
+++ brand/content/market-research.php
@@ -2,10 +2,10 @@
 <h2 id="why" class="heading-1">2. Market Research</h2>
 <h3 class="heading-2">2.1. Business Analysis</h3>
 <h4 class="heading-3">2.1.1. Overview</h4>
-<p>We're a full-service creative and development agency. We help businesses reach their customers with data-driven research and strategic storytelling.</p>
+<p>The Polyfen Group LLC is a small company parent of multiple brands related to software development and the creative industry.</p>
+<p>Our main brand is "Polyfen", a creative and development agency helping businesses reach their customers with data-driven research and strategic storytelling.</p>
 <h4 class="heading-3">2.1.2. Business Model</h4>
-<h4 class="heading-3">2.1.3. Services</h4>
-<h4 class="heading-3">2.1.4. Positioning</h4>
+<img src="imgs/polyfen-product-canvas.png" alt="Polyfen Product Canvas">
 <h3 class="heading-2">2.2. Industry Analysis</h3>
 <h3 class="heading-2">2.3. Target Analysis</h3>
 </section>
