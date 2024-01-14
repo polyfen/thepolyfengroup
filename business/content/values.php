e0d612ecf2bbd99803086572b81c693a991ed172
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-14 21:01:14 +0100

    Update values.php

--- business/content/values.php
+++ business/content/values.php
@@ -1,8 +1,11 @@
 <section id="values">
     <h2 id="core-values" class="heading-1">3. Values</h2>
-    <ul>
-        <li>Education</li>
-        <li>Collaboration</li>
-        <li>Transparency</li>
-    </ul>
+    <h3 class="heading-2">3.1. Education</h3>
+    <p>Innovation through R&amp;D. As a forward-thinking company we explore the latest techologies through internal Research & Development projects. That way, our clients count with the advantage of our expert know-how to beat the competition and never be left behind in an always moving market.</p>
+    <h3 class="heading-2">3.2. Collaboration</h3>
+    <p>#</p>
+    <h3 class="heading-2">3.3. Flexibility</h3>
+    <p>Fully Remote since 2018. We know that we can't compete with the comp packages of big tech companies. But where we can compete and win, is in flexibility. That allows us to have the best talent in the world at a sustainable cost.</p>
+    <h3 class="heading-2">3.4. Transparency</h3>
+    <p>Transparency is in our DNA. Our Business Plan and Team Wiki are just some of the internal documentation that is publically available. We also contribute to the open-source community with public code repositories in Github.</p>
 </section>
