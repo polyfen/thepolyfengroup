51365aca36e76e36e22be34edb5d934cc1802316
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-14 22:30:47 +0100

    Update values.php

--- business/content/values.php
+++ business/content/values.php
@@ -3,7 +3,7 @@
     <h3 class="heading-2">3.1. Education</h3>
     <p>Innovation through R&amp;D. As a forward-thinking company we explore the latest techologies through internal Research & Development projects. That way, our clients count with the advantage of our expert know-how to beat the competition and never be left behind in an always moving market.</p>
     <h3 class="heading-2">3.2. Collaboration</h3>
-    <p>#</p>
+    <p>Teamwork makes the deam work. Together we can achieve much more than isolated by ourselves.</p>
     <h3 class="heading-2">3.3. Flexibility</h3>
     <p>Fully Remote since 2018. We know that we can't compete with the comp packages of big tech companies. But where we can compete and win, is in flexibility. That allows us to have the best talent in the world at a sustainable cost.</p>
     <h3 class="heading-2">3.4. Transparency</h3>
