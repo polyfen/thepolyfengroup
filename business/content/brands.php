56e8906d9a00e7d4ab140291ad4779ab50006921
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-14 20:26:37 +0100

    Logos update

--- business/content/brands.php
+++ business/content/brands.php
@@ -16,16 +16,8 @@
       <a href="https://polyatlas.wiki" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyatlas.wiki</a>
     </section>
     </section>
-    <section id="poly-forum">
-    <h3 class="heading-2">4.3. Poly Forum</h3>
-    <section class="sub-brand">
-      <img src="imgs/sub-brands/poly-forum-logo.svg" alt="Poly Forum" height="60px" width="auto">
-      <p>Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry.</p>
-      <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyforum.org</a>
-    </section>
-    </section>
     <section id="poly-cookies">
-    <h3 class="heading-2">4.4. Poly Cookies</h3>
+    <h3 class="heading-2">4.3. Poly Cookies</h3>
     <section class="sub-brand">
       <img src="imgs/sub-brands/poly-cookies-logo.svg" alt="Poly Cookies" height="60px" width="auto">
       <p>Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager.</p>
@@ -33,7 +25,7 @@
     </section>
     </section>
     <section id="poly-qwerty">
-        <h3 class="heading-2">4.5. Poly Qwerty</h3>
+        <h3 class="heading-2">4.4. Poly Qwerty</h3>
         <section class="sub-brand">
         <img src="imgs/sub-brands/poly-qwerty-logo.svg" alt="Poly Qwerty" height="60px" width="auto">
         <p>Your keyboard's assistant. Click on a character to copy it.</p>
@@ -41,12 +33,31 @@
         </section>
     </section>
     <section id="poly-kit">
-        <h3 class="heading-2">4.6. Poly Kit</h3>
+        <h3 class="heading-2">4.5. Polykit</h3>
         <section class="sub-brand">
         <img src="imgs/sub-brands/poly-kit-logo.svg" alt="Poly Kit" height="60px" width="auto">
-        <p>Open-source design system... in early development. Very — early — development..</p>
+        <small class="caption" style="opacity: 0.6;"><br><br>// Work in Progress //</small>
+        <p>Open-source design system for User Interfaces with CSS code for developers and Figma library for designers.</p>
       <a href="https://polykit.xyz" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polykit.xyz</a>
         </section>
+    </section>
+    <section id="poly-press">
+    <h3 class="heading-2">4.6. Polypress</h3>
+    <section class="sub-brand">
+      <img src="imgs/sub-brands/polypress-logo.svg" alt="Poly Forum" height="60px" width="auto">
+      <small class="caption" style="opacity: 0.6;"><br><br>// Work in Progress //</small>
+      <p>Wordpress theme featuring out design system Polykit. Built for easy maintenance and scalibility with Elementor's page builder.</p>
+      <a href="https://polypress.xyz" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polypress.xyz</a>
+    </section>
+    </section>
+    <section id="poly-forum">
+    <h3 class="heading-2">4.7. Poly Forum</h3>
+    <section class="sub-brand">
+      <img src="imgs/sub-brands/poly-forum-logo.svg" alt="Poly Forum" height="60px" width="auto">
+      <small class="caption" style="opacity: 0.6;"><br><br>// Work in Progress //</small>
+      <p>Non-profit organization dedicated to promoting free and open education and professional growth in the creative industry.</p>
+      <a href="https://polyforum.org" target="_blank" class="web-url"><img src="imgs/icons/globe.svg" class="web-icon">polyforum.org</a>
+    </section>
     </section>
 </section>
 
