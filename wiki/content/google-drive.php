d23ec7b7d00046da6cf1f375a2a4b2d9046193f4
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-02-28 01:49:35 +0100

    Added embed code of Figjam to Google Drive section

--- wiki/content/google-drive.php
+++ wiki/content/google-drive.php
@@ -1,4 +1,4 @@
 <section id="google-drive">
     <h2 class="heading-1">10. Google Drive</h2>
-    <img src="imgs/google-drive-architecture.png">
+    <iframe style="border: 1px solid rgba(0, 0, 0, 0.1);" width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2F1Va1EE7o5DLbHGT8LNv5xq%2FGoogle-Drive%3Ftype%3Dwhiteboard%26node-id%3D8%253A541%26t%3D8XWj1bU7xBRCsBHY-1" allowfullscreen></iframe>
 </section>
