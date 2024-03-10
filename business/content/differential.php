84d98d33ded8333165702cfda426a7b3515ca846
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-11 00:34:59 +0100

    hotfix

--- business/content/differential.php
+++ business/content/differential.php
@@ -1,6 +1,6 @@
 <section id="differential">
     <h2 class="heading-1">7. Differential</h2>
-    <h3 class="heading-2">7.1. Low-cost, High-value</h3>
+    <h3 class="heading-2">7.1. Low cost, high value</h3>
     <p>Our cost-efficient, fully-remote business model offers services from top-tier professionals around the world at a small fraction of the price from our competitors in the US.</p>
     <h3 class="heading-2">7.2. Owner-managed reliability</h3>
     <p>Our agency is independently owned and our owners are involved from start to finish in all the projects we perform ensuring the best results for every client.</p>
