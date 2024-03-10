b4599833ec7dde53abd1514d88ac0e56eed70dc6
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-10 23:50:03 +0100

    Update differential.php

--- business/content/differential.php
+++ business/content/differential.php
@@ -5,7 +5,7 @@
     <h3 class="heading-2">7.2. Owner-managed reliability</h3>
     <p>Our agency is independently owned and our owners are involved from start to finish in all the projects we perform ensuring the best results for every client.</p>
     <h3 class="heading-2">7.3. End-to-end service</h3>
-    <p>We've got you covered next time you encounter any new requirements in the area of business development, marketing, design, or tech. Our growing list of satisfied customers is testimony that we don't just do the bare minimum. We go above and beyond to help your business grow and to develop a long-lasting and solid partnership.</p>
+    <p>We've got you covered next time you encounter any new requirements in the area of business development, marketing, design, or tech. Our growing list of satisfied customers is testimony that we go above and beyond to help your business grow and to develop a long-lasting and solid partnership.</p>
     <h3 class="heading-2">7.4. Fast-track kick-off</h3>
     <p>Our standardized and automated workflows ensure that no time is wasted. Send us an enquiry and we'll promptly engage in understanding your requirements, proposing a solution detailed in a comprehensive agreement, and prepare for a prompt project kick-off.</p>
 </section>
