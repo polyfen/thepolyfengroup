8b2726e52b53e42aafe8d72dedcf1b3e4b84787c
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-11 00:54:44 +0100

    fix: content update

--- business/content/differential.php
+++ business/content/differential.php
@@ -2,8 +2,8 @@
     <h2 class="heading-1">7. Differential</h2>
     <h3 class="heading-2">7.1. Low cost, high value</h3>
     <p>Our cost-efficient, fully-remote business model offers services from top-tier professionals around the world at a small fraction of the price from our competitors in the US.</p>
-    <h3 class="heading-2">7.2. Owner-managed reliability</h3>
-    <p>Our agency is independently owned and our owners are involved from start to finish in all the projects we perform ensuring the best results for every client.</p>
+    <h3 class="heading-2">7.2. Wealth of experience</h3>
+    <p>Founder &amp; Art Director <a href="https://boris.hr" rel="follow" target="_blank">Boris Hrnčić</a> brings to the table 10+ years of experience in design, development, and marketing. Vetted in the top 3% of designers worldwide by <a href="https://toptal.com" rel="nofollow" target="_blank">Toptal</a> in 2017. Led high-performing teams in for small start-ups and big enterprises in Croatia, Argentina, and the US.</p>
     <h3 class="heading-2">7.3. End-to-end service</h3>
     <p>We've got you covered next time you encounter any new requirements in the area of business development, marketing, design, or tech. Our growing list of satisfied customers is testimony that we go above and beyond to help your business grow and to develop a long-lasting and solid partnership.</p>
     <h3 class="heading-2">7.4. Fast-track kick-off</h3>
