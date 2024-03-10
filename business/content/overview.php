e8570b41a018d33428311ad5dcb1b24a26bb4d2d
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-10 18:45:09 +0100

    Content update

--- business/content/overview.php
+++ business/content/overview.php
@@ -2,7 +2,7 @@
 <h2 class="heading-1">1. Overview</h2>
   <p><strong>The Polyfen Group LLC</strong> is a business parent to multiple brands related to software development and the creative industry.</p>
   <p>We explore new ways of using tech in creative projects.</p>
-  <p>The company was founded in 2018 by <a href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a> &mdash;Argentinian-Croatian, art director and product designer&mdash; with the support of colleagues sharing the same passion for building something we can be proud of.</p>
+  <p>The company was founded on March 23rd, 2018 by <a href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a> &mdash;Argentinian-Croatian, art director &amp; product designer&mdash; with the support of colleagues sharing the same passion for building something we that can be proud of.</p>
   <p>The stock is 100% independently owned by the team with no dependency on investors or any other kind of external funding.</p>
   <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
 </section>
--- business/content/values.php
+++ business/content/values.php
@@ -1,11 +1,17 @@
 <section id="values">
     <h2 id="core-values" class="heading-1">3. Values</h2>
     <h3 class="heading-2">3.1. Education</h3>
-    <p>Innovation through R&amp;D. As a forward-thinking company we explore the latest techologies through internal Research & Development projects. That way, our clients count with the advantage of our expert know-how to beat the competition and never be left behind in an always moving market.</p>
+    <p>Excellence through R&amp;D. As a forward-thinking company we explore the latest techologies and nurture professional growth through internal Research & Development projects, like <a href="https://polyqwerty.com" rel="follow" target="_blank">Poly Qwerty</a> and <a href="https://polykit.xyz" rel="follow" target="_blank">Polykit</a>. That way, our clients count with the advantage of our top-tier talent to beat the competition and never be left behind in an always moving market.</p>
     <h3 class="heading-2">3.2. Collaboration</h3>
-    <p>Teamwork makes the deam work. Together we can achieve much more than isolated by ourselves.</p>
+    <p>Teamwork makes the deam work. Together we can achieve much more than we ever could alone. We are heavily commited to providing our team with all the tools, resources, and support to maintain a healthy and thriving work environment.</p>
     <h3 class="heading-2">3.3. Flexibility</h3>
-    <p>Fully Remote since 2018. We know that we can't compete with the comp packages of big tech companies. But where we can compete and win, is in flexibility. That allows us to have the best talent in the world at a sustainable cost.</p>
+    <p>Fully-remote since 2018. We can't compete with the comp packages of big tech companies. But where we can compete and win, is in flexibility. That allows us to have the best talent in the world at a sustainable cost.</p>
     <h3 class="heading-2">3.4. Transparency</h3>
-    <p>Transparency is in our DNA. Our Business Plan and Team Wiki are just some of the internal documentation that is publically available. We also contribute to the open-source community with public code repositories in Github.</p>
+    <p>Transparency is in our DNA. Our Business Plan and Team Wiki are just some of our documentation that is publically available. We also contribute to the open-source software engineering community with public code repositories in <a href="https://github.com/orgs/polyfen/repositories" rel="nofollow" target="_blank">Github</a>. We believe that the world of business could learn from the practices of the open-source community. Sharing our work allows us to:</p>
+    <ul class="hanging-list">
+        <li>Reach more people with through content marketing.</li>
+        <li>Gain the interest of potential clients and talent.</li>
+        <li>Document, standardize, and optimize our workflows.</li>
+        <li>Give our team-members an accessible framework to provide feedback, share ideas, and have more participation in the direction of our team culture.</li>
+    </ul>
 </section>
