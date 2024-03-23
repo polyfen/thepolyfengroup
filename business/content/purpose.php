177b9d5c7df0824a8433d645cb1ffbab944f0630
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-23 21:29:21 +0100

    improve: content update

--- business/content/purpose.php
+++ business/content/purpose.php
@@ -1,5 +1,11 @@
 <section id="purpose">
 <h2 id="why" class="heading-1">2. Purpose</h2>
-<p>The Polyfen Group exists because we wanted to create a company that aims <strong>to offer each team-member their dream job.</strong></p>
+<div class="preview">
+    <h3 class="caption">Why do we exist?</h3>
+    <h3 class="heading-2">To offer each team-member their dream job</h3>
+</div>
 <p>We believe that focusing on developing a team culture that we can be proud of provides the ideal environment to grow, innovate, and push the boundries of what we can do.</p>
+<div class="align-center">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/u4ZoJKF_VuA?si=vnVDif6gV876ERGX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
 </section>
