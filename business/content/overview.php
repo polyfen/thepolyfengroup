143ccffa01c8f218b7ca24ef8501713b6a44866e
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-23 22:08:16 +0100

    improve: content update

--- business/content/overview.php
+++ business/content/overview.php
@@ -8,4 +8,5 @@
   <p>The company was founded on March 23rd, 2018 by <a href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a> &mdash;Argentinian-Croatian, art director &amp; product designer&mdash; with the support of colleagues sharing the same passion for building something that we can be proud of.</p>
   <p>The stock is 100% independently owned by the team with no dependency on investors or any other kind of external funding.</p>
   <p>Our location-independent business model takes advantage of the latest advancements in technology to work seamlessly with a fully-remote team years before the Covid pandemic made it the new normal.</p>
+  <p>We are a certificed LLC registered in the state of Delaware, USA and we offer our clients solutions in the same market as US based competitors at a fraction of their costs.</p>
 </section>
