f75121e443c4849eb7e69ed8d239336654540960
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-23 22:00:35 +0100

    improve: content update

--- business/content/mantra.php
+++ business/content/mantra.php
@@ -1,6 +1,7 @@
 <section id="mantra">
-    <h2 id="core-values" class="heading-1">4. Mantra</h2>
+    <h2 id="core-values" class="heading-1">4. Motto</h2>
     <div class="preview">
         <h3 class="heading-2">Create. Collaborate. Shine.</h3>
     </div>
+    <p>Our Motto distills the spirit of our Purpose and Core Values into a concrete few words.</p>
 </section>
