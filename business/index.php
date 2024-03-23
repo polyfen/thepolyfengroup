a7ba9b15acdad36f3e2113a2da7e9ebf59aab257
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-24 00:08:53 +0100

    improve: content update

--- business/index.php
+++ business/index.php
@@ -48,7 +48,7 @@
             <ul>
                 <li><a href="#low-cost-high-value" class="caption">7.1. Low cost, High vlaue</a></li>
                 <li><a href="#wealth-of-experience" class="caption">7.2. Wealth of Experience</a></li>
-                <li><a href="#wealth-of-experience" class="caption">7.3. High-Performance</a></li>
+                <li><a href="#high-performance" class="caption">7.3. High-Performance</a></li>
                 <li><a href="#end-to-end-service" class="caption">7.4. End-to-End Service</a></li>
                 <li><a href="#fast-track-kick-off" class="caption">7.5. Fast-track Kick-off</a></li>
                 <li><a href="#fast-track-kick-off" class="caption">7.6. Socially-cohesive Team</a></li>
