6d79400448ae2139801077fbdea8c3fe451fe2fb
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-24 16:48:25 +0100

    improve: content update

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
                 <a class="screen" href="https://polyfen.com" target="_blank" rel="dofollow">
                     <img src="imgs/sub-brands/polyfen-screen.webp" alt="Polyfen screenshot">
                 </a>
-                <p>Fully-remote Creative &amp; Software boutique offering design, marketing, and engineering services for US businesses.</p>
+                <p>Creative &amp; Software boutique offering design, marketing, and engineering services for US businesses.</p>
                 <a href="https://polyfen.com" target="_blank" class="links web-url" rel="dofollow"><img src="imgs/icons/globe.svg"
                         class="web-icon" alt="">polyfen.com</a>
             </section>
