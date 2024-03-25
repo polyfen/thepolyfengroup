dabf47fedfa31c3ebc77953236be07c2c583f97c
xxiicode <leandrobilokapic@gmail.com>
2024-03-25 18:03:59 +0100

    Update family-of-brands.php

--- includes/family-of-brands.php
+++ includes/family-of-brands.php
@@ -1,7 +1,9 @@
 <section id="polyfen" class="sub-brand">
     <a class="screen" href="https://polyfen.com" target="_blank" rel="dofollow" title="polyfen.com">
-        <img src="/imgs/window-frame.svg" alt="" class="window-frame">
-        <img src="/imgs/sub-brands/polyfen-screen.webp" alt="Polyfen screenshot" class="web-screenshot">
+        <figure>
+            <img src="/imgs/window-frame.svg" alt="" class="window-frame">
+            <img src="/imgs/sub-brands/polyfen-screen.webp" alt="Polyfen screenshot" class="web-screenshot">
+        </figure>
     </a>
     <h3 class="polyfen --sub-brand">
         <a href="https://polyfen.com" target="_blank" class="web-url" rel="dofollow">Polyfen<span></span></a>
@@ -16,8 +18,10 @@
         <a href="https://polyatlas.wiki" target="_blank" class="web-url" rel="dofollow">Polyatlas<span></span></a>
     </h3>
     <a class="screen" href="https://polyatlas.wiki" target="_blank" rel="dofollow" title="polyatlas.wiki">
-        <img src="/imgs/window-frame.svg" alt="" class="window-frame">
-        <img src="/imgs/sub-brands/polyatlas-screen.webp" alt="Polyatlas screenshot" class="web-screenshot">
+        <figure>
+            <img src="/imgs/window-frame.svg" alt="" class="window-frame">
+            <img src="/imgs/sub-brands/polyatlas-screen.webp" alt="Polyatlas screenshot" class="web-screenshot">
+        </figure>
     </a>
     <p>Open-source step-by-step guide to the Branding process, including practical tips, case studies,
         template files, and many other useful resources.</p>
@@ -30,8 +34,10 @@
         <a href="https://polycookies.com" target="_blank" class="web-url" rel="dofollow">Polycookies<span></span></a>
     </h3>
     <a class="screen" href="https://polycookies.com" target="_blank" rel="dofollow" title="polycookies.com">
-        <img src="/imgs/window-frame.svg" alt="" class="window-frame">
-        <img src="/imgs/sub-brands/polycookies-screen.webp" alt="Polycookies screenshot" class="web-screenshot">
+        <figure>
+            <img src="/imgs/window-frame.svg" alt="" class="window-frame">
+            <img src="/imgs/sub-brands/polycookies-screen.webp" alt="Polycookies screenshot" class="web-screenshot">
+        </figure>
     </a>
     <p>Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager.
     </p>
@@ -44,8 +50,10 @@
         <a href="https://polyqwerty.com" target="_blank" class="web-url" rel="dofollow">polyqwerty<span></span></a>
     </h3>
     <a class="screen" href="https://polyqwerty.com" target="_blank" rel="dofollow" title="polyqwerty.com">
-        <img src="/imgs/window-frame.svg" alt="" class="window-frame">
-        <img src="/imgs/sub-brands/polyqwerty-screen.webp" alt="Polyqwerty screenshot" class="web-screenshot">
+        <figure>
+            <img src="/imgs/window-frame.svg" alt="" class="window-frame">
+            <img src="/imgs/sub-brands/polyqwerty-screen.webp" alt="Polyqwerty screenshot" class="web-screenshot">
+        </figure>
     </a>
     <p>Your keyboard's assistant. Click on a character to copy it. Easily find the right emoji, punctuation signs, and
         other symbols.</p>
@@ -58,8 +66,10 @@
         <a href="https://polykit.xyz" target="_blank" class="web-url" rel="dofollow">polykit<span></span></a>
     </h3>
     <a class="screen" href="https://polykit.xyz" target="_blank" rel="dofollow" title="polykit.xyz">
-        <img src="/imgs/window-frame.svg" alt="" class="window-frame">
-        <img src="/imgs/sub-brands/polykit-screen.webp" alt="Polikit screenshot" class="web-screenshot">
+        <figure>
+            <img src="/imgs/window-frame.svg" alt="" class="window-frame">
+            <img src="/imgs/sub-brands/polykit-screen.webp" alt="Polikit screenshot" class="web-screenshot">
+        </figure>
     </a>
     <p><small class="caption" style="opacity: 0.6;">// Work in Progress //</small><br><br>Open-source design system for
         User Interfaces with CSS code for developers and Figma library for
@@ -75,13 +85,15 @@
         <a href="https://polyforum.org/" target="_blank" class="web-url" rel="dofollow">polyforum<span></span></a>
     </h3>
     <a class="screen" href="https://polyforum.org/" target="_blank" rel="dofollow" title="polyforum.org">
-        <img src="/imgs/window-frame.svg" alt="" class="window-frame">
-        <img src="/imgs/sub-brands/polyforum-screen.webp" alt="Poliforum screenshot" class="web-screenshot">
+        <figure>
+            <img src="/imgs/window-frame.svg" alt="" class="window-frame">
+            <img src="/imgs/sub-brands/polyforum-screen.webp" alt="Poliforum screenshot" class="web-screenshot">
+        </figure>
     </a>
     <p><small class="caption" style="opacity: 0.6;">// Work in Progress //</small><br><br>TBD
     </p>
-    <a href="https://polyforum.org/" target="_blank" class="links web-url" rel="dofollow"><img src="/imgs/icons/globe.svg"
-            class="web-icon" alt="">polyforum.org</a>
+    <a href="https://polyforum.org/" target="_blank" class="links web-url" rel="dofollow"><img
+            src="/imgs/icons/globe.svg" class="web-icon" alt="">polyforum.org</a>
 </section>
 <hr>
 <section id="polypress" class="sub-brand">
@@ -89,11 +101,13 @@
         <a href="https://polykit.xyz" target="_blank" class="web-url" rel="dofollow">polypress<span></span></a>
     </h3>
     <a class="screen" href="https://polypress.xyz" target="_blank" rel="dofollow" title="polypress.xyz">
-        <img src="/imgs/window-frame.svg" alt="" class="window-frame">
-        <img src="/imgs/sub-brands/polypress-screen.webp" alt="Polipress screenshot" class="web-screenshot">
+        <figure>
+            <img src="/imgs/window-frame.svg" alt="" class="window-frame">
+            <img src="/imgs/sub-brands/polypress-screen.webp" alt="Polipress screenshot" class="web-screenshot">
+        </figure>
     </a>
     <p><small class="caption" style="opacity: 0.6;">// Work in Progress //</small><br><br>TBD
     </p>
-    <a href="https://polypress.xyz" target="_blank" class="links web-url" rel="dofollow"><img src="/imgs/icons/globe.svg"
-            class="web-icon" alt="">polypress.xyz</a>
+    <a href="https://polypress.xyz" target="_blank" class="links web-url" rel="dofollow"><img
+            src="/imgs/icons/globe.svg" class="web-icon" alt="">polypress.xyz</a>
 </section>
