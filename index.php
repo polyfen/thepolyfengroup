1bfaaaaac355cf359a5342c97d0440b5d1999d8b
xxiicode <leandrobilokapic@gmail.com>
2024-03-25 18:44:07 +0100

    add: hover effect to sub-brand screens

--- index.php
+++ index.php
@@ -61,8 +61,10 @@
             <hr>
             <section class="sub-brand">
                 <a class="screen" href="https://polyfen.com" target="_blank" rel="dofollow" title="polyfen.com">
-                    <img src="imgs/window-frame.svg" alt="" class="window-frame">
-                    <img src="imgs/sub-brands/polyfen-screen.webp" alt="Polyfen screenshot" class="web-screenshot">
+                    <figure>
+                        <img src="imgs/window-frame.svg" alt="" class="window-frame">
+                        <img src="imgs/sub-brands/polyfen-screen.webp" alt="Polyfen screenshot" class="web-screenshot">
+                    </figure>
                 </a>
                 <h3 class="polyfen --sub-brand">
                     <a href="https://polyfen.com" target="_blank" class="web-url" rel="dofollow">Polyfen<span></span></a>
@@ -77,8 +79,10 @@
                     <a href="https://polyatlas.wiki" target="_blank" class="web-url" rel="dofollow">Polyatlas<span></span></a>
                 </h3>
                 <a class="screen" href="https://polyatlas.wiki" target="_blank" rel="dofollow" title="polyatlas.wiki">
+                 <figure>
                     <img src="imgs/window-frame.svg" alt="" class="window-frame">
                     <img src="imgs/sub-brands/polyatlas-screen.webp" alt="Polyatlas screenshot" class="web-screenshot">
+                </figure>
                 </a>
                 <p>Open-source step-by-step guide to the Branding process, including practical tips, case studies,
                     template files, and many other useful resources.</p>
@@ -91,8 +95,10 @@
                     <a href="https://polycookies.com" target="_blank" class="web-url" rel="dofollow">Polycookies<span></span></a>
                 </h3>
                 <a class="screen" href="https://polycookies.com" target="_blank" rel="dofollow" title="polycookies.com">
+                <figure>
                     <img src="imgs/window-frame.svg" alt="" class="window-frame">
                     <img src="imgs/sub-brands/polycookies-screen.webp" alt="Polycookies screenshot" class="web-screenshot">
+                </figure>
                 </a>
                 <p>Open-source solution to privacy policy implementing a cookie consent banner with Google Tag Manager.
                 </p>
@@ -105,8 +111,10 @@
                     <a href="https://polyqwerty.com" target="_blank" class="web-url" rel="dofollow">polyqwerty<span></span></a>
                 </h3>
                 <a class="screen" href="https://polyqwerty.com" target="_blank" rel="dofollow" title="polyqwerty.com">
+                <figure>
                     <img src="imgs/window-frame.svg" alt="" class="window-frame">
                     <img src="imgs/sub-brands/polyqwerty-screen.webp" alt="Polyqwerty screenshot" class="web-screenshot">
+                 </figure>
                 </a>
                 <p>Your keyboard's assistant. Click on a character to copy it. Easily find the right emoji, punctuation signs, and other symbols.</p>
                 <a href="https://polyqwerty.com" target="_blank" class="links web-url" rel="dofollow"><img src="imgs/icons/globe.svg"
@@ -118,8 +126,10 @@
                     <a href="https://polykit.xyz" target="_blank" class="web-url" rel="dofollow">polykit<span></span></a>
                 </h3>
                 <a class="screen" href="https://polykit.xyz" target="_blank" rel="dofollow" title="polykit.xyz">
+                <figure>
                     <img src="imgs/window-frame.svg" alt="" class="window-frame">
                     <img src="imgs/sub-brands/polykit-screen.webp" alt="Polikit screenshot" class="web-screenshot">
+                </figure>
                 </a>
                 <p><small class="caption" style="opacity: 0.6;">// Work in Progress //</small><br><br>Open-source design system for User Interfaces with CSS code for developers and Figma library for
                     designers.
