57b5c0fc81d0841e0b8a538fa49e1bfc6d7352c1
xxiicode <leandrobilokapic@gmail.com>
2024-04-10 17:03:14 +0200

    add: new content for Dev Workflows on commits

--- wiki/content/development-workflow.php
+++ wiki/content/development-workflow.php
@@ -1,6 +1,6 @@
 <section id="development-workflow">
     <h2 class="heading-1">12. Development Workflow</h2>
-    <p>Website project set-up:</p>
+    <h3 class="heading-2">12.1. Website project set-up:</h3>
     <ol class="hanging-list">
         <li>Create Github repository</li>
         <li>Buy web domain</li>
@@ -19,4 +19,13 @@
         <li>Add Poly Cookies</li>
         <li>Create and add Favicon system</li>
     </ol>
+    <h3 class="heading-2">12.2. Prefixes conventions on Github commits</h3>
+    <ul class="hanging-list">
+        <li><strong>add:</strong> Used when adding new features, functionalities, or content to the project.  <i>"add: contact form to the website."</i></li>
+        <li><strong>fix:</strong> Employed when correcting bugs, errors, or issues within the project.  <i>"fix: alignment of accordion icons."</i></li>
+        <li><strong>improve:</strong> Indicates enhancements or optimizations made to existing features or functionalities.  <i>"improve: loading speed of homepage images."</i></li>
+        <li><strong>remove:</strong> Signifies the removal or deletion of features, functionalities, or content from the project.  <i>"remove: outdated card images."</i></li>
+        <li><strong>hotfix:</strong> Reserved for urgent fixes deployed quickly to resolve critical issues in production.  <i>"hotfix"</i>.</li>
+    </ul>
+
 </section>
