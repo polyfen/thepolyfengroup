f59ad918df7d134b10b9f3400738dd74fc82664c
xxiicode <leandrobilokapic@gmail.com>
2024-04-19 11:35:05 +0200

    improve: workflow section en la /Wiki

--- wiki/content/development-workflow.php
+++ wiki/content/development-workflow.php
@@ -1,6 +1,6 @@
 <section id="development-workflow">
     <h2 class="heading-1">11. Development Workflow</h2>
-    <h3 class="heading-2">11.1. Website project set-up:</h3>
+    <h3 class="heading-2"  id="website-project-set-up">11.1. Website Project Set-up</h3>
     <ol class="hanging-list">
         <li>Create Github repository</li>
         <li>Buy web domain</li>
@@ -19,13 +19,17 @@
         <li>Add Poly Cookies</li>
         <li>Create and add Favicon system</li>
     </ol>
-    <h3 class="heading-2">11.2. Prefixes conventions on Github commits</h3>
+    <h3 class="heading-2"  id="github-commits">11.2. Github Commits</h3>
     <ul class="hanging-list">
         <li><strong>add:</strong> Used when adding new features, functionalities, or content to the project.  <i>"add: contact form to the website."</i></li>
         <li><strong>fix:</strong> Employed when correcting bugs, errors, or issues within the project.  <i>"fix: alignment of accordion icons."</i></li>
         <li><strong>improve:</strong> Indicates enhancements or optimizations made to existing features or functionalities.  <i>"improve: loading speed of homepage images."</i></li>
         <li><strong>remove:</strong> Signifies the removal or deletion of features, functionalities, or content from the project.  <i>"remove: outdated card images."</i></li>
         <li><strong>hotfix:</strong> Reserved for urgent fixes deployed quickly to resolve critical issues in production.  <i>"hotfix"</i>.</li>
+        <li><strong>Content Update:</strong> Used when making updates or revisions to the content of the project, such as text, images, or multimedia. <i>"content update"</i>.</li>
     </ul>
+
+    <h3 class="heading-2"  id="changelog-update">11.3. ChangeLog Update</h3>
+    <p>After completing a task, it's important to update the <a href="https://log.thepolyfengroup.com/" target="_blank" rel="dofollow">ChangeLog</a> with the implemented changes. This update should not include hotfixes or content updates.</p>
 
 </section>
