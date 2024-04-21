fbbbe3b9b827cf1f69772fc46e436439b1c962c9
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-21 21:33:28 +0200

    content update

--- brand/content/content-generation.php
+++ brand/content/content-generation.php
@@ -5,6 +5,7 @@
 <p>The Newsletter is the holy grail of our content strategy. It allows us to focus on short-form content delivered directly to our subscribers in their inbox.</p>
 <p>Most Blogs out there demand a lot of resources to end up with a very long, elaborate, polished post, that none or very few people read in full. Our Newsletter prioritizes an efficient content creation workflow that results in easily degistible content published frequently and consistently.</p>
 <p>The content produced for the Newsletter is repurposed in two ways. (A) as Social Media posts in LinkedIn, and (B) kept in an online archive as a page in <a data-before="plfn" href="https://polyfen.com/newsletter" target="_blank" rel="dofollow">Polyfen's</a> website.</p>
+<p>The purpose of the Newsletter is not to push sales of our services blatantly. The goal is to provide our followers with valuable information about the industry.</p>
 <h4 class="heading-3" id="subject">Subject</h4>
 <ul class="hanging-list">
     <li>Start with an emoji to illustrate the topic.</li>
@@ -30,9 +31,14 @@
 <h4 class="heading-3" id="body-text">Body Text</h4>
 <ul class="hanging-list">
     <li>Ideally 3-5 paragraphs long.</li>
+    <li>Finish the message with a link to our LinkedIn page.</li>
 </ul>
 <p>Example</p>
 <div class="preview">
-    <p style="text-align:left;">#</p>
+    <p style="text-align:left;">Ever marveled at how certain brands effortlessly command higher prices for their products or services? It's not just luck – it's the strategic power of brand equity at play.</p>
+    <p style="text-align:left;">Let's dive into two key aspects:</p>
+    <p style="text-align:left;">1. Premium Pricing: Imagine strolling into a boutique and eyeing a sleek, minimalist watch. Sure, there are cheaper options out there, but you're drawn to this one because of the brand behind it. That's the allure of premium pricing fueled by brand equity. When customers perceive your brand as top-tier, they're not just buying a product – they're investing in a promise of quality, reliability, and status. And that's worth every penny.</p>
+    <p style="text-align:left;">2. Company Value: Now, picture this: You're in the boardroom, pitching your company to potential investors. As you showcase your impressive revenue figures and growth projections, there's one intangible asset that steals the spotlight – your brand. A well-crafted brand isn't just a logo or a tagline; it's a strategic asset that can significantly boost the value of your company. Investors know that a strong brand translates to customer loyalty, market dominance, and long-term sustainability – all factors that drive up company valuation. Whether you're seeking new investors or looking to increase the value of shares for existing stakeholders, your brand is your secret weapo</p>
+    <p style="text-align:left;">If you're ready to unlock the full potential of your brand and elevate your company to new heights of success, let's chat. Together, we'll harness the power of brand equity to drive premium pricing and skyrocket your company's value.</p>
 </div>
 </section>
