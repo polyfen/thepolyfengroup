98d95273d01755da6015ff2ab7bd67727adeb97a
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-21 19:11:44 +0200

    content update

--- brand/content/content-generation.php
+++ brand/content/content-generation.php
@@ -1,8 +1,8 @@
 <section id="content-generation">
 <h2 class="heading-1">7. Content Generation</h2>
+<p>The subject matter of all content generation must be relevant, congruent with our <a href="#voice" data-emoji="anchor">Voice</a> and <a href="#tone" data-emoji="anchor">Tone</a>, and in touch with reality. We don't want to be <a href="https://www.reddit.com/r/LinkedInLunatics/" target="_blank" rel="nofollow" title="reddit.com/r/linkedinlunatics" data-emoji="link">r/LinkedInLunatics</a>. We aim to publish content frequently, yes; But, never at the expense of quality.</p>
 <h3 id="newsletter" class="heading-2">7.1. Newsletter</h3>
 <p>The Newsletter is the holy grail of our content strategy. It allows us to focus on short-form content delivered directly to our subscribers in their inbox.</p>
 <p>Most Blogs out there demand a lot of resources to end up with a very long, elaborate, polished post, that none or very few people read in full. Our Newsletter prioritizes an efficient content creation workflow that results in easily degistible content published frequently and consistently.</p>
 <p>The content produced for the Newsletter is repurposed in two ways. (A) as Social Media posts in LinkedIn, and (B) kept in an online archive as a page in <a data-before="plfn" href="https://polyfen.com/newsletter" target="_blank" rel="dofollow">Polyfen's</a> website.</p>
-<p>The subject matter must always be relevant, congruent with our Core Values and our Tone, and in touch with reality. We don't want to be <a href="https://www.reddit.com/r/LinkedInLunatics/" target="_blank" rel="nofollow" title="reddit.com/r/linkedinlunatics" data-emoji="link">r/LinkedInLunatics</a>. We aim to publish content frequently, yes; But, never at the expense of quality.</p>
 </section>
