65e813bc79f0aad034b69a6ee5f17d715986b380
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-05-20 18:46:43 +0200

    add: terms and conditions

---
+++ terms-and-conditions/includes/head.php
@@ -0,0 +1,42 @@
+<!-- META General -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta property="og:type" content="website"/>
+<meta name="author"  content="The Polyfen Group LLC"/>
+
+<!-- CSS General -->
+<link rel="stylesheet" href="https://polykit.xyz/polynucleus/polynucleus.css" type="text/css">
+<link rel="stylesheet" href="https://polykit.xyz/polynucleus/theme/docs-theme-v2.css" type="text/css">
+
+<!-- scripts -->
+<script defer src="https://polykit.xyz/polynucleus/theme/docs-mobile-nav-v2.js"></script>
+<script defer src="app/privacy-scroll.js"></script>
+
+<!-- FONTS -->
+<!--
+/**
+* @license
+* MyFonts Webfont Build ID 568563
+*
+* The fonts listed in this notice are subject to the End User License
+* Agreement(s) entered into by the website owner. All other parties are
+* explicitly restricted from using the Licensed Webfonts(s).
+*
+* You may obtain a valid license from one of MyFonts official sites.
+* http://www.fonts.com
+* http://www.myfonts.com
+* http://www.linotype.com
+*
+*/
+-->
+<link rel="stylesheet" type="text/css" href="app/fonts/Biotif.css">
+
+
+<!-- FAVICON -->
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#7537f8">
+<meta name="msapplication-TileColor" content="#7537f8">
+<meta name="theme-color" content="#7537f8">
---
+++ terms-and-conditions/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Terms & Conditions | Polyfen</title>
+
+    <!-- INCLUDE HEAD.PHP start -->
+    <?php include 'includes/head.php';?>
+    <!-- INCLUDE HEAD.PHP end -->
+
+  </head>
+
+  <body>
+    <nav id="navbar">
+    <a href="https://thepolyfengroup.com" rel="dofollow" class="link-logo">
+      <img class="plfn-logo" src="imgs/the-polyfen-group-logo.svg" alt="The Polyfen group" height="40px" width="auto">
+    </a>
+      <h2 class="caption">Terms & Conditions</h2>
+      <ul class="toc">
+        <li><a href="#introduction" class="caption">1. Introduction</a></li>
+        <li><a href="#company-information" class="caption">2. Company Information</a></li>
+        <li><a href="#subscription-plans" class="caption">3. Subscription Plans</a></li>
+        <li><a href="#payment-and-billing" class="caption">4. Payment & Billing</a></li>
+        <li><a href="#cancellation-policy" class="caption">5. Cancellation Policy</a></li>
+        <li><a href="#refund-polict" class="caption">6. Refund Policy</a></li>
+        <li><a href="#intellectual-property" class="caption">7. Intellectual Property</a></li>
+        <li><a href="#limitation-of-liability" class="caption">8. Limitation of Liability & Warranties</a></li>
+        <li><a href="#support-and-communication" class="caption">9. Support & Communication</a></li>
+        <li><a href="#privacy-and-data" class="caption">10. Privacy & Data Protection</a></li>
+        <li><a href="#acceptable-use" class="caption">11. Acceptable Use</a></li>
+        <li><a href="#amendments-to-tems" class="caption">12. Amendments to Terms</a></li>
+        <li><a href="#governing-law" class="caption">13. Governing Law</a></li>
+        <li><a href="#dispute-resolution" class="caption">14. Dispute Resolution</a></li>
+        <li><a href="#contact-information" class="caption">15. Contact Information</a></li>
+        <br><br>
+      </ul>
+    </nav>
+    <button aria-label="Open/Close navbar" class="hamburger">
+        <img src="imgs/icons/hamburger_icon.svg" alt="" height="30px" width="auto">
+    </button>
+    <button class="close">
+        <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24">
+            <path fill="none" stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="m16 16l-4-4m0 0L8 8m4 4l4-4m-4 4l-4 4" />
+        </svg>
+    </button>
+    <main>
+      <section class="cover">
+        <h1 class="title-2">Terms & Conditions</h1>
+        <h2 class="caption">The Polyfen Group LLC<br>Est. 2018</h2>
+      </section>
+        <hr>
+        <!-- INCLUDE CONTENT.PHP start  -->
+        <?php include 'content/content.php';?>
+        <!-- INCLUDE CONTENT.PHP end -->
+        <hr>
+    </main>
+  </body>
+</html>
