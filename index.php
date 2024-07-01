73af645937f265209f218082356db1ea7ce444b8
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-07-01 19:56:58 +0200

    #

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
     <!-- End Google Tag Manager -->
 </head>
 
-<body class="home">
+<body class="light-mode polyfen-site home">
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PBRK5MR5" height="0" width="0"
             style="display:none; visibility:hidden"></iframe></noscript>
@@ -46,28 +46,31 @@
     <?php include 'includes/hellobar.php';?>
     <?php include 'includes/navbar.php';?>
     <!-- INCLUDE HELLO's end -->
-    <div class="home-container">
-        <header class="home">
-            <!-- <div class="logo-container"> -->
-                <img src="imgs/the-polyfen-group-ideogram.svg" id="tpg-ideogram" alt="">
-                <img src="imgs/the-polyfen-group-wordmark.svg" id="tpg-wordmark" alt="">
-            <!-- </div> -->
-        </header>
-        <main class="home">
-            <section id="home-cover">
-                <h1 class="title-1">We play with tech</h1>
-                <p>The Polyfen Group is a small company parent of multiple brands in the software development
-                    and
-                    the creative industries.</p>
-            </section>
-            <section class="sub-brands-container">
-                <h2 class="heading-1">Family of Brands</h2>
-                <hr>
-                <?php include './includes/family-of-brands.php'; ?>
-                <hr>
-            </section>
-        </main>
-    </div>
+
+    <header>
+        <!-- <div class="logo-container"> -->
+            <img src="imgs/the-polyfen-group-ideogram.svg" id="tpg-ideogram" alt="">
+            <img src="imgs/the-polyfen-group-wordmark.svg" id="tpg-wordmark" alt="">
+        <!-- </div> -->
+    </header>
+
+    <main>
+        <section class="home-cover">
+            <h1 class="title-1">We play with tech</h1>
+            <p>The Polyfen Group is a small company parent of multiple brands in the software development
+                and
+                the creative industries.</p>
+        </section>
+        <section class="container sub-brands">
+            <h2 class="heading-1">Family of Brands</h2>
+            <hr>
+            <?php include './includes/family-of-brands.php'; ?>
+            <hr>
+        </section>
+    </main>
+
+    <script src="https://polykit.xyz/app/js/polykit-v0-3.js"></script>
+
 </body>
 
 </html>
