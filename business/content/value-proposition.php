5d6a1df70aa6b0829df03ab279acbbddae83939d
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-09-13 14:22:43 +0200

    Update value-proposition.php

--- business/content/value-proposition.php
+++ business/content/value-proposition.php
@@ -1,22 +1,22 @@
 <section id="value-proposition">
     <h2 class="heading-1">6. Value Proposition</h2>
     <hr class="small-separator">
-    <h3 id="low-cost-high-value" class="heading-2">7.1. Low cost, high value</h3>
+    <h3 id="low-cost-high-value" class="heading-2">6.1. Low cost, high value</h3>
     <p>Our cost-efficient, fully-remote business model provides services from top-tier professionals around the world at a small fraction of the price from our competitors in the US.</p>
     <hr class="small-separator">
-    <h3 id="owner-managed" class="heading-2">7.2. Owner-managed</h3>
+    <h3 id="owner-managed" class="heading-2">6.2. Owner-managed</h3>
     <p>Polyfen's owners are are fully immersed in every project ensuring the best results for each and every client.</p>
     <hr class="small-separator">
-    <h3 id="end-to-end-service" class="heading-2">7.3. End-to-end service</h3>
+    <h3 id="end-to-end-service" class="heading-2">6.3. End-to-end service</h3>
     <p>Our growing list of satisfied customers is testimony that we go above and beyond. Whatever you need, we've got you covered.</p>
     <hr class="small-separator">
-    <h3 id="fast-track-kick-off" class="heading-2">7.4. Fast-track kick-off</h3>
+    <h3 id="fast-track-kick-off" class="heading-2">6.4. Fast-track kick-off</h3>
     <p>Our workflows ensure no time is wasted. Tell us what you need and we're good to go.</p>
     <hr class="small-separator">
-    <h3 id="top-tier-talent" class="heading-2">7.5. Top-tier talent</h3>
+    <h3 id="top-tier-talent" class="heading-2">6.5. Top-tier talent</h3>
     <p>We recruit only the best of the best. Qualified geniuses, passion-driven pros.</p>
     <hr class="small-separator">
-    <h3 id="near-shore-availability" class="heading-2">7.6. Near-shore availability</h3>
+    <h3 id="near-shore-availability" class="heading-2">6.6. Near-shore availability</h3>
     <p>As opposed to off-shore teams in India, our team is fully available in US timezones.</p>
 </section>
 
