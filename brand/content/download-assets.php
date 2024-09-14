5ba15a91afac3763e490f3f5f4f62823779a4be2
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-09-14 23:00:26 +0200

    Update download-assets.php

--- brand/content/download-assets.php
+++ brand/content/download-assets.php
@@ -2,6 +2,7 @@
 <h2 class="heading-1">1. Download Assets</h2>
 <p>The Media Kit is public and includes our logo system por partners and press to use on their website and other media.</p>
 <p>The Brand Assets is a private folder for internal use only and includes licensed artwork and files.</p>
+<hr class="small-separator">
 <h3 class="heading-2">1.1. Media Kit <span class="font-weight-400 italic">(public)</span></h3>
 <div class="preview">
     <a href="https://drive.google.com/drive/folders/1hEl8370cqLWeeUEWjiC05Api3KhNaxgS?usp=sharing" target="_blank" class="button primary-button large-button">Media Kit</a>
