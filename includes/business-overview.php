0742a41f90626eb57bfc6f52c28adb50406abe15
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-09-28 17:31:32 -0300

    Content update

--- includes/business-overview.php
+++ includes/business-overview.php
@@ -2,13 +2,13 @@
 <h3 class="heading-3">Tech + Creative</h3>
 </div>
 <p><strong>The Polyfen Group</strong> is a fully remote tech and creative company. We're established in the US, but most of our team-members live in Argentina.</p>
-<p>The primary brand we operate is <strong><a data-before="plfn" href="https://polyfen.com" target="_blank" rel="follow">Polyfen</a></strong>, a software and creative studio. But our brand identity system is built to support and encourage growth with other products and services as sub-brands under <strong><a data-before="plfn" href="https://thepolyfengroup.com" target="_blank" rel="follow">The Polyfen Group's</a></strong> umbrella.</p>
-<p>Polyfen offers software and creative services with three types of billing:</p>
+<p>The primary brand we operate is <strong><a data-before="plfn" href="https://polyfen.com" target="_blank" rel="follow">Polyfen</a></strong>, a software and creative studio. But our brand identity system is built to foster growth with other products and services as sub-brands under <strong><a data-before="plfn" href="https://thepolyfengroup.com" target="_blank" rel="follow">The Polyfen Group's</a></strong> umbrella.</p>
+<p>Polyfen offers software and creative solutions with three types of billing:</p>
 <ul>
     <li><strong>Hire:</strong> staff augmentation with hourly billing.</li>
     <li><strong>Subscribe:</strong> delivery team with monthly billing.</li>
     <li><strong>Build:</strong> delivery team with fixed-price project billing.</li>
 </ul>
 <p>Our location-independent business model takes advantage of the latest tech to work seamlessly with a fully remote team years before the COVID pandemic made it the new normal. While many companies are struggling with office space costs and back to the office campaigns we're still enojying the freedom and flexibility of a fully remote team that knows how to collaborate and create extraodinary work from wherever they'd like to be.</p>
-<p>Our culture is built around R&amp;D. For the best software and creative studio "One size fits all" solutions don't cut it. We invest half of our resources into incubating digital products that maximise efficiency in our workflows and maintain us at the leading edge of the industry.</p>
-<p>The company was founded on March 23rd, 2018 by art director, product designer <a data-before="bh" href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a>. In early 2024 the business incorporated new partners and has since been growing steadily in headcount.</p>
+<p>Our culture is built around R&amp;D. For the best software and creative studio "One size fits all" solutions don't cut it. We invest half of our resources into incubating digital products that maximise efficiency in our workflows and sustain us at the leading edge of the industry.</p>
+<p>The company was founded on March 23rd, 2018 by Chief Design Officer, <a data-before="bh" href="https://boris.hr" target="_blank">Boris Hrn&ccaron;i&cacute;</a>. In early 2024 the business incorporated new partners and has since been growing steadily in headcount.</p>
