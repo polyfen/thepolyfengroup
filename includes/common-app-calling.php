49523dae0cc1eef93ce142c317139a6cd01e8daf
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-12-30 17:48:15 +0100

    Fix

--- includes/common-app-calling.php
+++ includes/common-app-calling.php
@@ -1,6 +1,6 @@
 
 <!-- POLYKIT -->
-<link rel="stylesheet" href="https://polykit.xyz/app/css/polykit-v0-4.css" type="text/css">
+<link rel="stylesheet" href="../app/polykit-v0-4.css" type="text/css">
 <link rel="stylesheet" href="../app/styles-new.css" type="text/css">
 
 <!-- FONTS -->
